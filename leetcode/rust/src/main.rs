34acfe8aa4f7d7800698a4b10a287a0343db39de
andooown <andooown.dev@gmail.com>
2023-05-21 20:12:29 +0900

    347. Top K Frequent Elements

--- leetcode/rust/src/main.rs
+++ leetcode/rust/src/main.rs
@@ -1,5 +1,6 @@
 mod contains_duplicate;
 mod group_anagrams;
+mod top_k_frequent_elements;
 mod two_sum;
 mod valid_anagram;
 
---
+++ leetcode/rust/src/top_k_frequent_elements.rs
@@ -0,0 +1,56 @@
+use super::Solution;
+
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Count {
+    value: i32,
+    count: i32,
+}
+
+impl Solution {
+    pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
+        let mut counts = Solution::count(nums);
+        counts.sort_unstable_by(|lhs, rhs| lhs.count.cmp(&rhs.count).reverse());
+
+        counts.iter().take(k as usize).map(|c| c.value).collect()
+    }
+
+    fn count(nums: Vec<i32>) -> Vec<Count> {
+        let mut counts = HashMap::new();
+        for n in nums {
+            counts.entry(n).and_modify(|c| *c += 1).or_insert(1);
+        }
+
+        counts
+            .iter()
+            .map(|(k, v)| Count {
+                value: *k,
+                count: *v,
+            })
+            .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Solution;
+    use std::collections::HashSet;
+
+    #[test]
+    fn top_k_frequent() {
+        assert_eq!(
+            HashSet::from_iter(Solution::top_k_frequent(vec![1, 1, 1, 2, 2, 3], 2)),
+            HashSet::from([1, 2])
+        );
+        assert_eq!(
+            HashSet::from_iter(Solution::top_k_frequent(vec![1], 1)),
+            HashSet::from([1])
+        );
+
+        assert_eq!(
+            HashSet::from_iter(Solution::top_k_frequent(vec![4, 1, -1, 2, -1, 2, 3], 2)),
+            HashSet::from([-1, 2])
+        );
+    }
+}
