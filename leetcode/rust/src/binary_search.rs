e4a9bda399e33ae53adc4194eb5dea3e77c3e890
andooown <andooown.dev@gmail.com>
2023-05-30 22:24:06 +0900

    Use standard library

--- leetcode/rust/src/binary_search.rs
+++ leetcode/rust/src/binary_search.rs
@@ -2,27 +2,7 @@ struct Solution;
 
 impl Solution {
     pub fn search(nums: Vec<i32>, target: i32) -> i32 {
-        Solution::recursive_search(&nums, target).map_or(-1, |i| i as i32)
-    }
-
-    fn recursive_search(nums: &[i32], target: i32) -> Option<usize> {
-        println!("{nums:?}");
-        match nums.len() {
-            0 => None,
-            1 => {
-                if nums[0] == target {
-                    Some(0)
-                } else {
-                    None
-                }
-            }
-            len => match nums[len / 2] {
-                v if v == target => Some(len / 2),
-                v if v < target => Solution::recursive_search(&nums[len / 2 + 1..], target)
-                    .map(|i| i + len / 2 + 1),
-                _ => Solution::recursive_search(&nums[..len / 2], target),
-            },
-        }
+        nums.binary_search(&target).map(|v| v as i32).unwrap_or(-1)
     }
 }
 
